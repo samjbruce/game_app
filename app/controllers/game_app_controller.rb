417ca916f006a9228af44ffa0b074f2eac1ea20a
samjbruce <samueljamesbruce@gmail.com>
2021-05-06 13:52:40 -0500

    Adds name_game and number_game methods

--- app/controllers/game_app_controller.rb
+++ app/controllers/game_app_controller.rb
@@ -1,5 +1,28 @@
 class GameAppController < ApplicationController
-  def name
-    render json: {name: "This is your name"}
+  
+  def name_game
+    name = params[:name]
+    name_array = name.split("")
+    if name_array[0] == "a"
+      name_capitalized = name.upcase
+      render json: {
+        name: name_capitalized,
+        message: "Hey, your name starts with the first letter of the alphabet!"
+      }
+    else 
+      name_capitalized = name.upcase
+      render json: {name: name_capitalized}
+    end
+  end
+
+  def number_game
+    number = params[:number].to_i
+    if number == 35
+     render json: {message: "That is correct!"}
+    elsif number < 35
+      render json: {message: "Higher! Keep trying."}
+    elsif number > 35
+      render json: {message: "Lower! Keep trying."}
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
-  get "/name" => "game_app#name"
+  get "/name_game" => "game_app#name_game"
+  get "/number_game" => "game_app#number_game"
 end
